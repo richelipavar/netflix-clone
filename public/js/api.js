dc7fc7286bbd06c802703eebc139653763cf47f3
Richeli Pavarini Mascarenha <91886144+richelipavar@users.noreply.github.com>
2022-12-22 17:47:52 -0300

    commit init
    Primeiro commit do site

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "5ed6d332e221934563de3cd4e5a6bc6c";
+
+let img_url = "https://image.tmdb.org/t/p/w500/";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
